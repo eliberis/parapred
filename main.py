6eee4479afb6db963e0715fb2032346b7807a81c
Edgar Liberis <edgaras.liberis@gmail.com>
2017-03-19 21:53:13 +0000

    Move plotting to its own file

--- main.py
+++ main.py
@@ -1,54 +1,7 @@
 from data_provider import open_dataset
 from model import get_model
-from sklearn import metrics
+from plotting import *
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-def plot_accuracies(history):
-    plt.interactive(False)
-
-    plt.plot(history.history['precision'])
-    plt.plot(history.history['val_precision'])
-    plt.title('model precision')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig("prec.png")
-
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig("loss.png")
-
-def plot_prec_rec_curve(model, ags_test, examples_test, labels_test):
-    plt.interactive(False)
-
-    abip_rec = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.92])
-    abip_pre = \
-        np.array([0.78, 0.74, 0.66, 0.62, 0.56, 0.51, 0.5, 0.48, 0.45, 0.44])
-
-    test_probabilities = model.predict([ags_test, examples_test])
-    prec, rec, thresholds = metrics.precision_recall_curve(
-        labels_test.flatten(), test_probabilities.flatten())
-
-    # Maximum interpolation
-    for i in range(len(prec)):
-        prec[i] = prec[:(i+1)].max()
-
-    plt.plot(rec, prec)
-    plt.plot(abip_rec, abip_pre)
-
-    plt.ylabel("Precision")
-    plt.yticks(np.linspace(0.3, 1, 8))
-
-    plt.xlabel("Recall")
-    plt.xticks(np.linspace(0.0, 1, 11))
-
-    plt.savefig("proc.png")
 
 def main():
     ags, cdrs, lbls, params = open_dataset()
